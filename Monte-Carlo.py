82b064e50f92f3f432fa114714b10b518c4cacd6
Steftet <Steftet@users.noreply.github.com>
2017-02-02 17:42:36 +0100

    Update Monte-Carlo.py

--- Monte-Carlo.py
+++ Monte-Carlo.py
@@ -3,7 +3,7 @@ import random
 
 h = 0
 
-k = int(input('Anzahl Punkte: '))
+k = int(input('Number of dots: '))
 for i in range(0,k):
     x = (random.randint(0,100)/100)
     y = (random.randint(0,100)/100)
